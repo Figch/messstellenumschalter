d60ce36e4f96e7561c70a35b0a23a9ff3dc4a225
Fisch <interfisch@gmx.net>
2020-08-27 19:27:17 +0200

    buttons and test prints

--- messumschalter/messumschalter.ino
+++ messumschalter/messumschalter.ino
@@ -2,6 +2,17 @@
 #define PIN_CLOCK 12 //74HC595 Pin 11 SH_CP (clock) connected to Arduino D12
 #define PIN_DATA 11 //74HC595 Pin 14 DS (data) connected to Arduino D11
 
+#define PIN_BUTTON A0
+#define PIN_LED A1
+
+#define PIN_DIP1 7
+#define PIN_DIP2 6
+#define PIN_DIP3 5
+#define PIN_DIP4 4
+
+unsigned long last_button_down = 0;
+#define BUTTON_DEBOUNCETIME 200
+
 byte data;
 #define ARRAYSIZE 2
 byte dataArray[ARRAYSIZE];
@@ -13,44 +24,57 @@ void setup() {
   pinMode(PIN_CLOCK, OUTPUT);
   pinMode(PIN_DATA, OUTPUT);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  pinMode(PIN_LED, OUTPUT); digitalWrite(PIN_LED, HIGH);
+  pinMode(LED_BUILTIN, OUTPUT); digitalWrite(LED_BUILTIN, HIGH);
+  
+  pinMode(PIN_DIP1, INPUT_PULLUP);
+  pinMode(PIN_DIP2, INPUT_PULLUP);
+  pinMode(PIN_DIP3, INPUT_PULLUP);
+  pinMode(PIN_DIP4, INPUT_PULLUP);
+  
   Serial.begin(9600);
 
   switchOffAllRelais();
+  
+  digitalWrite(PIN_LED, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
 
-/*
-  dataArray[0]=0x00;
-  dataArray[1]=0x00;
-  updateShiftregister();
-  Serial.println("off");
-  digitalWrite(LED_BUILTIN, LOW);
+  boolean buttonFlag = false;
+  if (!digitalRead(PIN_BUTTON)) { //button pressed
+    digitalWrite(PIN_LED, HIGH);
+    if (millis() - last_button_down > BUTTON_DEBOUNCETIME) {
+      buttonFlag = true;
+    }
+    last_button_down=millis();
+  }
+  else {
+    digitalWrite(PIN_LED, LOW);
+  }
 
-  delay(1000);
-
-  dataArray[0]=0xFF;
-  dataArray[1]=0xFF;
-
-  updateShiftregister();
+  if (buttonFlag) {
+    Serial.println("Button!");
+    Serial.println(digitalRead(PIN_DIP1));
+    Serial.println(digitalRead(PIN_DIP2));
+    Serial.println(digitalRead(PIN_DIP3));
+    Serial.println(digitalRead(PIN_DIP4));
+  }
   
-  Serial.println("on");
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  delay(1000);
-  */
 
   
-
   Serial.print("output="); Serial.println(selectedOutput);
   selectRelais(selectedOutput);
   Serial.println(dataArray[0], BIN);
   Serial.println(dataArray[1], BIN);
-  delay(1000);
+  delay(100);
 
   selectedOutput++,
   selectedOutput = selectedOutput % 16;
+
+  
   
 }
 
